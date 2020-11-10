0c99347b4d6707c179137b31fffcfbdc4da978d2
Patel210 <pranavpatel2101997@gmail.com>
2020-11-10 14:12:46 +0530

    Added: Pincode can have one space in between

--- regex.js
+++ regex.js
@@ -1,5 +1,5 @@
 function validatePinCode(pinCode) {
-    let pinRegex = RegExp('^[1-9][0-9]{5}$');
+    let pinRegex = RegExp('^[1-9][0-9]{2}[\\s]{0,1}[0-9]{3}$');
     if (pinRegex.test(pinCode)) {
         console.log("Hurray! Valid Pin Code");
     } else {
@@ -14,4 +14,6 @@ validatePinCode(pinCode);
 pinCode = "A400088";
 validatePinCode(pinCode);
 pinCode = "400088B";
+validatePinCode(pinCode);
+pinCode = "400 088";
 validatePinCode(pinCode);
