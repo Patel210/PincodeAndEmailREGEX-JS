2b20f55bc766fc9e8d700cd99b75857774e9117d
Patel210 <pranavpatel2101997@gmail.com>
2020-11-10 14:51:01 +0530

    Added: Sample Emails To Verify

--- regex.js
+++ regex.js
@@ -8,7 +8,7 @@ function validatePinCode(pinCode) {
 }
 
 function validateEmail(email) {
-    let emailRegex = RegExp("^[a-z0-9]+(([\\.+-_][a-z0-9]{1,})?)+@[a-z0-9]+\\.([a-z]{2,4})+((\\.[a-z]{2})?)$");
+    let emailRegex = RegExp("^[a-z0-9]+(([\\.+-][a-z0-9]{1,})?)+@[a-z0-9]+\\.([a-z]{2,4})+((\\.[a-z]{2,4})?)$");
     if (emailRegex.test(email)) {
         console.log("Valid Email!");
     } else {
@@ -29,3 +29,9 @@ validatePinCode(pinCode);
 
 let email = "abc.xyz@bridgelabz.co.in";
 validateEmail(email);
+
+let arrays = ["abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", 
+"abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com", "abc", "abc@.com.my", "abc123@gmail.a", 
+"abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", 
+"abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au"];
+arrays.forEach(validateEmail);
