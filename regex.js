babe87f46006400bb9de2e7e907570c1fdc4fced
Patel210 <pranavpatel2101997@gmail.com>
2020-11-10 14:28:44 +0530

    Added: Ability to verify the mandatory '.' after mandatory '@'

--- regex.js
+++ regex.js
@@ -8,7 +8,7 @@ function validatePinCode(pinCode) {
 }
 
 function validateEmail(email) {
-    let emailRegex = RegExp('^[a-z]{3,}[@][a-z]{3,}$');
+    let emailRegex = RegExp('^[a-z]{3,}[@][a-z]{3,}[.][a-z]{2,4}$');
     if (emailRegex.test(email)) {
         console.log("Valid Email!");
     } else {
@@ -27,5 +27,5 @@ validatePinCode(pinCode);
 pinCode = "400 088";
 validatePinCode(pinCode);
 
-let email = "abc@bridgelabz";
+let email = "abc@bridgelabz.co";
 validateEmail(email);
